8c0d08e873d6a84f19436ec12e3e3362b4a64626
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-02-20 20:49:34 +0200

    tested DRF

---
+++ api_server/user/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import TestModel
+
+
+
+
+class TestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TestModel
+        fields = ['name']
