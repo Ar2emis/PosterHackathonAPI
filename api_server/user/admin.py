640d9435c973e02534c8083d28da185f80bdf693
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-02-21 12:39:40 +0200

    added models

--- api_server/user/admin.py
+++ api_server/user/admin.py
@@ -1,3 +1,42 @@
-from django.contrib import admin
+from django.contrib import admin, auth
+from .models import *
 
-# Register your models here.
+
+admin.site.unregister(auth.models.Group)
+admin.site.unregister(auth.models.User)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WatchType)
+class WatchTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+class WatchTasksInline(admin.TabularInline):
+    model = Watch.tasks.through
+    verbose_name = 'task'
+
+
+@admin.register(Watch)
+class WatchAdmin(admin.ModelAdmin):
+    inlines = [WatchTasksInline]
+    exclude = ['tasks']
--- api_server/user/models.py
+++ api_server/user/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    uid = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    uid = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=40)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.role} {self.name}'
+
+
+class WatchType(models.Model):
+    uid = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Progress(models.Model):
+    uid = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.TextField()
+    worker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    progress = models.ForeignKey(Progress, on_delete=models.SET_DEFAULT, default=Progress.objects.filter(uid='todo').first().pk, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Watch(models.Model):
+    date = models.DateField(primary_key=True)
+    watch_type = models.ForeignKey(WatchType, on_delete=models.SET_NULL, null=True)
+    tasks = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return f'{self.date} {self.watch_type.name}'
+
